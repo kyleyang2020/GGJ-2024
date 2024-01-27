d29f19e0d13652cbb45d5da5cb46e3633ce7676c
Kyle Yang <kyleyang2020@gmail.com>
2024-01-27 10:46:08 -0500

    add mini game layout additively

---
+++ Assets/MiniGameManger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniGameManger : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SceneManager.UnloadScene(2);
+        }
+    }
+}
