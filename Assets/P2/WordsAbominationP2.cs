da8b6b09a2b9e2bab98b172fdd19d0fbf2d63b9b
Kyle Yang <kyleyang2020@gmail.com>
2024-01-27 16:14:16 -0500

    words and choices are properly displayed
    - buttons still do nothing

--- Assets/P2/WordsAbominationP2.cs
+++ Assets/P2/WordsAbominationP2.cs
@@ -1,16 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using TMPro;
-using UnityEditor;
-using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WordsAbominationP2 : MonoBehaviour
 {
     // list of 5 letter words in play
-    List<string> list = new List<string> { "obama", "chaos", "tears", "ducks", "queen" };
+    List<string> list = new List<string> { "obama", "chaos", "tears", "ducks", "missippi" };
 
     // word with missing letter and it's letter
     char[] wordMissOne;
@@ -19,21 +16,51 @@ public class WordsAbominationP2 : MonoBehaviour
     // TMPro text object
     public TMPro.TextMeshProUGUI guessWord;
 
+    // list of buttons/answers
+    public List<Button> buttonList;
+    public List<TMPro.TextMeshProUGUI> buttonTextList;
+
+    // int of the correct answer
+    int randomButtonText;
+
     private void Start()
     {
+        // show the word with missing letter
         guessWord.text = string.Join("  ", GetRandomWordLetters(list));
+
+        // random button and its text from 0-4
+        randomButtonText = UnityEngine.Random.Range(0, buttonTextList.Count);
+        buttonTextList[randomButtonText].text = letter.ToString();
+
+        // 97-122, a-z
+        for(int i = 0; i < buttonTextList.Count; i++)
+        {
+            // skips over the actual answer
+            if (i == randomButtonText)
+                i++;
+            // randomly chooses a lower case letter to fill in for wrong answers
+            // DID NOT IMPLEMENT EDGE CASES where correct answers can come up
+            else
+                buttonTextList[i].text = Convert.ToChar(UnityEngine.Random.Range(97, 122)).ToString();
+        }
+    }
+
+    private void Update()
+    {
+        // NEED TO IMPLEMENT BUTTON CLICK ACTIONS
+        Button correctAnswer = buttonList[randomButtonText];
     }
 
     char[] GetRandomWordLetters(List<string> words)
     {
         // gets random word and set it
-        int randomWordIndex = Random.Range(0, words.Count);
+        int randomWordIndex = UnityEngine.Random.Range(0, words.Count);
 
         // puts random word into a array of characters
         char[] chars = words[randomWordIndex].ToCharArray();
 
         // replace the missing letter with '_'
-        int missingLetter = Random.Range(0, chars.Length);
+        int missingLetter = UnityEngine.Random.Range(0, chars.Length);
         letter = chars[missingLetter]; // set missing letter char
 
         // changing said letter to '_'
